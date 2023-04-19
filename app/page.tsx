835e0624ed85c5be015cb4f3950d2ce319354f39
Francis Lagares <francis.lagares@gmail.com>
2023-04-19 19:16:33 +0200

    feat: create RestaurantCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import Link from 'next/link';
-
 import Header from './components/Header';
 import NavBar from './components/NavBar';
+import RestaurantCard from './components/RestaurantCard';
 
 const Home = () => {
   return (
@@ -10,34 +9,9 @@ const Home = () => {
         <NavBar />
         <main>
           <Header />
-          {/* CARDS */}
           <div className='py-3 px-36 mt-10 flex flex-wrap justify-center'>
-            {/* CARD */}
-            <div className='w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer'>
-              <Link href='/restaurant/milestones-grill'>
-                <img
-                  src='https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg'
-                  alt=''
-                  className='w-full h-36'
-                />
-                <div className='p-1'>
-                  <h3 className='font-bold text-2xl mb-2'>Milestones Grill</h3>
-                  <div className='flex items-start'>
-                    <div className='flex mb-2'>*****</div>
-                    <p className='ml-2'>77 reviews</p>
-                  </div>
-                  <div className='flex text-reg font-light capitalize'>
-                    <p className=' mr-3'>Mexican</p>
-                    <p className='mr-3'>$$$$</p>
-                    <p>Toronto</p>
-                  </div>
-                  <p className='text-sm mt-1 font-bold'>Booked 3 times today</p>
-                </div>
-              </Link>
-            </div>
-            {/* CARD */}
+            <RestaurantCard />
           </div>
-          {/* CARDS */}
         </main>
       </main>
     </main>
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -11,7 +11,7 @@ const RestaurantDetails = () => {
         <div className='h-96 overflow-hidden'>
           <div className='bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center'>
             <h1 className='text-7xl text-white captitalize text-shadow text-center'>
-              Milestones Grill (Toronto)
+              Milestones Grill
             </h1>
           </div>
         </div>
