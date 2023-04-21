feaab9e83f66c21cb50ebc4d3aef2cfee0531630
Francis Lagares <francis.lagares@gmail.com>
2023-04-21 18:50:38 +0200

    refactor: create ReservationCard component

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -4,6 +4,7 @@ import Description from './components/Description';
 import Header from './components/Header';
 import Images from './components/Images';
 import Rating from './components/Rating';
+import ReservationCard from './components/ReservationCard';
 import RestaurantNavBar from './components/RestaurantNavBar';
 import RestaurantTitle from './components/RestaurantTitle';
 import Reviews from './components/Reviews';
@@ -14,7 +15,6 @@ const RestaurantDetails = () => {
       <main className='max-w-screen-2xl m-auto bg-white'>
         <NavBar />
         <Header />
-        {/* DESCRIPTION PORTION */}
         <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
           <div className='bg-white w-[70%] rounded p-3 shadow'>
             <RestaurantNavBar />
@@ -24,11 +24,10 @@ const RestaurantDetails = () => {
             <Images />
             <Reviews />
           </div>
-          <div className='w-[27%] relative text-reg'></div>
+          <div className='w-[27%] relative text-reg'>
+            <ReservationCard />
+          </div>
         </div>
-        {/* DESCRIPTION PORTION */} {/* RESERVATION CARD PORTION */}{' '}
-        {/* RESERVATION
-    CARD PORTION */}
       </main>
     </main>
   );
