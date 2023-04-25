b7218c60d94047da2088618d0caa3671c4d5def2
Francis Lagares <francis.lagares@gmail.com>
2023-04-25 18:45:59 +0200

    refactor: server to client components

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,6 +1,6 @@
 import NavBar from '../components/NavBar';
+import Header from './components/Header';
 import RestaurantCard from './components/RestaurantCard';
-import SearchForm from './components/SearchForm';
 import SearchSideBar from './components/SearchSideBar';
 
 const Search = () => {
@@ -8,7 +8,7 @@ const Search = () => {
     <main className='bg-gray-100 min-h-screen w-screen'>
       <main className='max-w-screen-2xl m-auto bg-white'>
         <NavBar />
-        <SearchForm />
+        <Header />
         <div className='flex py-4 m-auto w-2/3 justify-between items-start'>
           <SearchSideBar />
           <div className='w-5/6'>
