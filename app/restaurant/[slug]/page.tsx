da025d6e230b387cde136fc7bad278e144f0d178
Francis Lagares <francis.lagares@gmail.com>
2023-04-26 17:53:51 +0200

    refactor: move duplicated html to layout

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,3 @@
-import NavBar from '@/app/components/NavBar';
-
 import Description from './components/Description';
 import Header from './components/Header';
 import Images from './components/Images';
@@ -11,25 +9,22 @@ import Reviews from './components/Reviews';
 
 const RestaurantDetails = () => {
   return (
-    <main className='bg-gray-100 min-h-screen w-screen'>
-      <main className='max-w-screen-2xl m-auto bg-white'>
-        <NavBar />
-        <Header />
-        <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-          <div className='bg-white w-[70%] rounded p-3 shadow'>
-            <RestaurantNavBar />
-            <RestaurantTitle />
-            <Rating />
-            <Description />
-            <Images />
-            <Reviews />
-          </div>
-          <div className='w-[27%] relative text-reg'>
-            <ReservationCard />
-          </div>
+    <>
+      <Header />
+      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
+        <div className='bg-white w-[70%] rounded p-3 shadow'>
+          <RestaurantNavBar />
+          <RestaurantTitle />
+          <Rating />
+          <Description />
+          <Images />
+          <Reviews />
         </div>
-      </main>
-    </main>
+        <div className='w-[27%] relative text-reg'>
+          <ReservationCard />
+        </div>
+      </div>
+    </>
   );
 };
 
