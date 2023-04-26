edb5a37d368358410fcc55332c7dba3a7e50b00d
Francis Lagares <francis.lagares@gmail.com>
2023-04-26 18:10:06 +0200

    refactor: create restaurant layout

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,4 @@
 import Description from './components/Description';
-import Header from './components/Header';
 import Images from './components/Images';
 import Rating from './components/Rating';
 import ReservationCard from './components/ReservationCard';
@@ -10,19 +9,16 @@ import Reviews from './components/Reviews';
 const RestaurantDetails = () => {
   return (
     <>
-      <Header />
-      <div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
-        <div className='bg-white w-[70%] rounded p-3 shadow'>
-          <RestaurantNavBar />
-          <RestaurantTitle />
-          <Rating />
-          <Description />
-          <Images />
-          <Reviews />
-        </div>
-        <div className='w-[27%] relative text-reg'>
-          <ReservationCard />
-        </div>
+      <div className='bg-white w-[70%] rounded p-3 shadow'>
+        <RestaurantNavBar />
+        <RestaurantTitle />
+        <Rating />
+        <Description />
+        <Images />
+        <Reviews />
+      </div>
+      <div className='w-[27%] relative text-reg'>
+        <ReservationCard />
       </div>
     </>
   );
