94f8a2dc87598ae64b376a2fce9f1a835e586b28
Francis Lagares <francis.lagares@gmail.com>
2023-07-12 14:41:08 +0200

    feat: fetch and display list of restaurants

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
-import './globals.css';
+import '@/styles/globals.css';
 
-import NavBar from './components/NavBar';
+import NavBar from '@/components/NavBar';
 
 export const metadata = {
   title: 'OpenTable',
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,38 @@
-import Header from './components/Header';
-import RestaurantCard from './components/RestaurantCard';
+import Header from '@/components/Header';
+import RestaurantCard from '@/components/RestaurantCard';
+import { Restaurant } from '@/models/restaurant';
+import { PrismaClient } from '@prisma/client';
 
-const Home = () => {
+const prisma = new PrismaClient();
+
+const fetchRestaurants = async (): Promise<Restaurant[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      slug: true,
+      mainImage: true,
+      cuisine: true,
+      location: true,
+      price: true,
+    },
+  });
+
+  return restaurants;
+};
+
+const Home = async () => {
+  const restaurants = await fetchRestaurants();
+
+  console.log({ restaurants });
+
   return (
     <>
       <Header />
       <div className='py-3 px-36 mt-10 flex flex-wrap justify-center'>
-        <RestaurantCard />
+        {restaurants.map(restaurant => (
+          <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+        ))}
       </div>
     </>
   );
