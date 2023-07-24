b5e24e38027f3d05e0b79ec177f69fa96930570b
Francis Lagares <francis.lagares@gmail.com>
2023-07-24 19:14:37 +0200

    feat: implement auth middleware

--- src/pages/api/auth/me.ts
+++ src/pages/api/auth/me.ts
@@ -1,31 +1,12 @@
 import { PrismaClient } from '@prisma/client';
-import * as jose from 'jose';
 import jwt from 'jsonwebtoken';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const prisma = new PrismaClient();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  const bearerToken = req.headers['authorization'];
-
-  if (!bearerToken) {
-    return res.status(401).json({ errorMessage: 'Unauthorized request.' });
-  }
-
+  const bearerToken = req.headers['authorization'] as string;
   const token = bearerToken.split(' ')[1];
-
-  if (!token) {
-    return res.status(401).json({ errorMessage: 'Unauthorized request.' });
-  }
-
-  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-
-  try {
-    await jose.jwtVerify(token, secret);
-  } catch (error) {
-    res.status(401).json({ errorMessage: 'Unauthorized request.' });
-  }
-
   const payload = jwt.decode(token) as { email: string };
 
   if (!payload.email) {
