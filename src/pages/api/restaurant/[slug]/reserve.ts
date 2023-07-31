fb4cf470a5b1d244391fed338d05adfb521d981e
Francis Lagares <francis.lagares@gmail.com>
2023-07-31 20:00:16 +0200

    feat: validate existing restaurant and opening hours

--- src/pages/api/restaurant/[slug]/reserve.ts
+++ src/pages/api/restaurant/[slug]/reserve.ts
@@ -1,4 +1,7 @@
+import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
+
+const prisma = new PrismaClient();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { slug, day, time, partySize } = req.query as {
@@ -7,6 +10,27 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     time: string;
     partySize: string;
   };
+
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (!restaurant) {
+    return res.json({
+      errorMessage: 'Restaurant not found.',
+    });
+  }
+
+  if (
+    new Date(`${day}T${time}`) < new Date(`${day}T${restaurant.openTime}`) ||
+    new Date(`${day}T${time}`) > new Date(`${day}T${restaurant.openTime}`)
+  ) {
+    res.status(400).json({
+      errorMessage: 'Restaurant is not open at that time.',
+    });
+  }
 
   return res.json({
     slug,
