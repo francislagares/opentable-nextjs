21ed6c82e7447edac4600e5ffd1d37a8e925bd32
Francis Lagares <francis.lagares@gmail.com>
2023-07-31 21:14:40 +0200

    feat: count tables based on seats

--- src/pages/api/restaurant/[slug]/reserve.ts
+++ src/pages/api/restaurant/[slug]/reserve.ts
@@ -1,6 +1,8 @@
 import { PrismaClient } from '@prisma/client';
 import { NextApiRequest, NextApiResponse } from 'next';
 
+import { findAvailableTables } from '@/services/findAvailableTables';
+
 const prisma = new PrismaClient();
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -14,6 +16,11 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const restaurant = await prisma.restaurant.findUnique({
     where: {
       slug,
+    },
+    select: {
+      tables: true,
+      openTime: true,
+      closeTime: true,
     },
   });
 
@@ -32,11 +39,49 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     });
   }
 
-  return res.json({
-    slug,
+  const searchTimesWithTables = await findAvailableTables({
     day,
     time,
-    partySize,
+    res,
+    restaurant,
+  });
+
+  if (!searchTimesWithTables) {
+    return res.status(400).json({
+      errorMessage: 'Invalid data provided.',
+    });
+  }
+
+  const searchTimeWithTables = searchTimesWithTables.find(schedule => {
+    return (
+      schedule.date.toISOString() === new Date(`${day}T${time}`).toISOString()
+    );
+  });
+
+  if (!searchTimeWithTables) {
+    return res.status(400).json({
+      errorMessage: 'No availability, cannot book.',
+    });
+  }
+
+  const tablesCount: {
+    2: number[];
+    4: number[];
+  } = {
+    2: [],
+    4: [],
+  };
+
+  searchTimeWithTables.tables.forEach(table => {
+    if (table.seats === 2) {
+      tablesCount[2].push(table.id);
+    } else {
+      tablesCount[4].push(table.id);
+    }
+  });
+
+  return res.json({
+    tablesCount,
   });
 };
 
