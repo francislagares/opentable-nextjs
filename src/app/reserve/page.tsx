7f4c2f617d77a5cb450dc6c302a800b9b6fb8dc0
Francis Lagares <francis.lagares@gmail.com>
2023-08-01 17:05:04 +0200

    feat: extract query params

--- src/app/reserve/page.tsx
+++ src/app/reserve/page.tsx
@@ -1,15 +1,55 @@
+import { PrismaClient } from '@prisma/client';
+import { notFound } from 'next/navigation';
+
+import { Params } from '../types/params';
 import Form from './components/Form';
 import Header from './components/Header';
 
-export const metadata = {
-  title: 'Reserve at Milestones Grill | OpenTable',
+export const generateMetadata = ({ params }: Params) => {
+  const { slug } = params;
+
+  return {
+    title: `${slug} | OpenTable`,
+  };
 };
 
-const Reserve = () => {
+const prisma = new PrismaClient();
+
+const fetchRestaurantBySlug = async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+  });
+
+  if (!restaurant) {
+    notFound();
+  }
+
+  return restaurant;
+};
+
+const Reserve = async ({
+  params,
+  searchParams,
+}: {
+  params: { slug: string };
+  searchParams: { date: string; partySize: string };
+}) => {
+  const { slug } = params;
+  console.log(slug);
+
+  const restaurant = await fetchRestaurantBySlug(slug);
+
   return (
     <div className='border-t h-screen'>
       <div className='py-9 w-3/5 m-auto'>
-        <Header />
+        <Header
+          image={restaurant.name}
+          name={restaurant.name}
+          date={searchParams.date}
+          partySize={searchParams.partySize}
+        />
         <Form />
       </div>
     </div>
